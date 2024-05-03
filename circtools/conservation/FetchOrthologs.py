046521fd97bf1b207c10eb2c02fa90306efdd608
shubhada-kulkarni <s.kulkarni@dkfz-heidelberg.de>
2024-05-03 10:37:28 +0200

    Change function arguments to call from main wrapper script

--- circtools/conservation/FetchOrthologs.py
+++ circtools/conservation/FetchOrthologs.py
@@ -1,22 +1,24 @@
 # This script is used to fetch the ortholog information per gene 
 # using the REST API by ENSMBLE
 
-import os, sys
+import sys
 import requests
 
 class fetch(object):
-    def __init__(self) -> None:
-        pass
 
-    def fetch_info(self, gene_symbol, from_species):
+    def __init__(self, gene_symbol, from_species) -> None:
+        self.gene_symbol = gene_symbol
+        self.from_species = from_species
+
+    def fetch_info(self):
         # define the species for which you are finding orthologs
         species = ["mouse", "human", "rat", "pig", "dog"]
-        to_species = list(set(species)-set([from_species]))
+        to_species = list(set(species)-set([self.from_species]))
         to_species = ["target_species="+str(i) for i in to_species]
         str_to_species = ";".join(to_species)
 
         server = "https://rest.ensembl.org"
-        ext = "/homology/symbol/" + from_species + "/" + gene_symbol + "?format=condensed;type=orthologues;" + str_to_species
+        ext = "/homology/symbol/" + self.from_species + "/" + self.gene_symbol + "?format=condensed;type=orthologues;" + str_to_species
  
         r = requests.get(server+ext, headers={ "Content-Type" : "application/json"})
  
@@ -34,6 +36,6 @@ class fetch(object):
         for each_key in ortho_dir.keys():
             print(each_key, )
 
-obj = fetch()
-obj.fetch_info("SLC8A1")
+#obj = fetch()
+#obj.fetch_info("SLC8A1")
 #fetch.fetch_info("SLC8A1")
