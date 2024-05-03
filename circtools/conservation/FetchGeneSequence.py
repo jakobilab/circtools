0b4b02e87ca9deda71245a0dee15ac7916cdcb89
shubhada-kulkarni <s.kulkarni@dkfz-heidelberg.de>
2024-05-03 14:14:27 +0200

    Class to fetch gene sequence from REST API

---
+++ circtools/conservation/FetchGeneSequence.py
@@ -0,0 +1,26 @@
+# This script is used to fetch the ortholog information per gene 
+# using the REST API by ENSMBLE
+
+import sys
+import requests
+
+class sequence(object):
+
+    def __init__(self, geneid) -> None:
+        self.geneid = geneid
+
+    def fetch_sequence(self):
+
+        server = "https://rest.ensembl.org"
+        ext = "/sequence/id/" + self.geneid + "?"
+ 
+        r = requests.get(server+ext, headers={ "Content-Type" : "text/plain"})
+ 
+        if not r.ok:
+            r.raise_for_status()
+            sys.exit() 
+        self.r = r
+        print(r.text)
+
+#obj = sequence("ENSCAFG00845024998")
+#obj.fetch_sequence()
