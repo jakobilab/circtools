fe64af776c96dc74df9e1b150215ec4014f4f534
shubhada-kulkarni <s.kulkarni@dkfz-heidelberg.de>
2024-05-22 12:02:50 +0200

    Liftover command working

--- circtools/conservation/LiftOver.py
+++ circtools/conservation/LiftOver.py
@@ -17,17 +17,18 @@ class liftover(object):
         self.from_id = species_IDs_dict[self.from_species]
         self.to_id = species_IDs_dict[self.to_species]
 
-        tmp_from_bed = self.tmpdir + self.prefix + "_circtools_flanking_exons.tmp"
+        tmp_from_bed = self.tmpdir + self.prefix + "_liftover.tmp"
         open(tmp_from_bed, 'w').close()             # erase old contents
         with open(tmp_from_bed, 'a') as data_store:
-            data_store.write("\t".join(self.from_coord))
+            data_store.write("chr" + "\t".join(self.from_coord) + "\n")
         # chain file
-        chain_file = self.from_id + "To" + self.to_id + ".over.chain.gz"
-        # output file
-        tmp_to_bed = tmp_from_bed + ".out.tmp"
-        # unlifted file
-        tmp_unlifted = tmp_from_bed + ".unlifted.tmp" 
+        chain_file = self.from_id + "To" + self.to_id.title() + ".over.chain.gz"
+        
+        tmp_to_bed = tmp_from_bed + ".out"              # output file
+        open(tmp_to_bed, 'a').close()                   # erase old contents
+        tmp_unlifted = tmp_from_bed + ".unlifted"       # unlifted file
+        open(tmp_unlifted, 'a').close()                   # erase old contents
 
         liftover_utility = "/home/skulkarni/liftOver"
-        command = liftover_utility + " " + chain_file + " " + tmp_to_bed + " " + tmp_unlifted 
+        command = liftover_utility + " " + tmp_from_bed + " " + chain_file + " " + tmp_to_bed + " " + tmp_unlifted + "  -multiple -minMatch=0.1"
         print(command)
