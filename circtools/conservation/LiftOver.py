1a3f508102394cb4e8313f4712837e5e4d2a7689
shubhada-kulkarni <s.kulkarni@dkfz-heidelberg.de>
2024-05-22 11:13:57 +0200

    Removed unwanted function + added more tmp file in arguments

--- circtools/conservation/LiftOver.py
+++ circtools/conservation/LiftOver.py
@@ -3,25 +3,24 @@
 
 class liftover(object):
 
-    def __init__(self, from_species, to_species) -> None:
+    def __init__(self, from_species, to_species, bed_coord, tmpdir, prefix) -> None:
         self.from_species = from_species
         self.to_species = to_species
-        self.gene_symbol = gene_symbol
         self.from_coord = bed_coord     # BED coordinates in form of a list of chr, start and stop, score and strand
-
-    def species_ids(self):
-        # define species IDs to be sent to the liftover function
-        species_IDs_dict = {"mouse":"mm10", "human":"hg38", "pig":"susScr11", "dog":"canFam6"}
-        self.from_id = species_IDs_dict[from_species]
-        self.to_id = species_IDs_dict[to_species]
+        self.tmpdir = tmpdir
+        self.prefix = prefix
 
     def lifting(self):
         # function to perform actual lifting
 
-        tmp_from_bed = self.temp_dir + tmp_prefix + "_circtools_flanking_exons.tmp"
+        species_IDs_dict = {"mouse":"mm10", "human":"hg38", "pig":"susScr11", "dog":"canFam6"}
+        self.from_id = species_IDs_dict[self.from_species]
+        self.to_id = species_IDs_dict[self.to_species]
+
+        tmp_from_bed = self.tmpdir + self.prefix + "_circtools_flanking_exons.tmp"
         open(tmp_from_bed, 'w').close()             # erase old contents
         with open(tmp_from_bed, 'a') as data_store:
-            data_store.write("\t".join(from_coord))
+            data_store.write("\t".join(self.from_coord))
         # chain file
         chain_file = self.from_id + "To" + self.to_id + ".over.chain.gz"
         # output file
