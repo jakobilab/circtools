298f7d7c0052a48559de4fffea7837eca2772ba6
Tobias Jakobi <tjakobi@arizona.edu>
2025-01-28 18:18:05 -0700

    Code cleanup

--- docker_path_wrapper.py
+++ docker_path_wrapper.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2025 Tobias Jakobi
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import sys
 import subprocess
 
 def process_paths(paths):
-    modified_paths = []
+    modified_paths_int = []
     for path in paths:
         if os.path.isabs(path):
             # Prefix absolute path with "/host_os"
             modified_path = os.path.join("/host_os", path.lstrip("/"))
-            modified_paths.append(modified_path)
+            modified_paths_int.append(modified_path)
         # not an (absolut) path, add unmodified
         else:
-            modified_paths.append(path)
-    return modified_paths
+            modified_paths_int.append(path)
+    return modified_paths_int
 
 if __name__ == "__main__":
-    # print(sys.argv)
     if len(sys.argv) < 2:
         sys.exit(1)
 
     user_paths = sys.argv[1:]
     modified_paths = process_paths(user_paths)
-
-    # print("DEBUG: " + " ".join(modified_paths))
-    #
-    # subprocess.run(["pwd"])
-    # subprocess.run(["ls", "-la"])
-    # subprocess.run(["ls", "/host_os/home/tjakobi/repos/jakobilab/long_read_circRNA/test_fastq/"])
 
     modified_paths.insert(0,'circtools')
 
