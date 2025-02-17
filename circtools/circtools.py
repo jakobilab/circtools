b4670116b210f01a50eeb6c3d63dc598b9fcde51
Tobias Jakobi <tjakobi@arizona.edu>
2025-02-16 17:12:38 -0700

    version and typo

--- circtools/circtools.py
+++ circtools/circtools.py
@@ -20,7 +20,7 @@ import os.path
 import sys
 
 # global settings
-version = "1.3.1"
+version = "2.0"
 program_name = "circtools"
 
 
@@ -59,7 +59,7 @@ class CircTools(object):
                reconstruct:  circular RNA reconstruction
                enrich:       circular RNA RBP enrichment scan
                exon:         circular RNA alternative exon analysis
-               conservation:  circular RNA conservation analysis
+               conservation: circular RNA conservation analysis
             """)
         parser.add_argument("command", help="Command to run")
 
